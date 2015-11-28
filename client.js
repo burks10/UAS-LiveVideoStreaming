adbb38539cff0f3b817f5ed33da4af7949a8b94d
Anthony Burkholder <hublr@outlook.com>
2015-11-28 18:42:05 -0500

    System has been re-implemented using Flask and AngularJS instead of NodeJS

--- client.js
+++ client.js
@@ -1,27 +1,30 @@
-var DEBUG_MODE = true;
-var client;
+var app = angular.module("uasApp", []);
+var count = 0;
 
+// Video Controller
+getFrame = app.controller("videoCtrl",
+    function($scope, $http, $interval) {
+        $interval(function() {
+            var img;
 
-if(!DEBUG_MODE){
-  client = new BinaryClient('ws://10.42.0.20:9000');
-}else{
-  client = new BinaryClient('ws://localhost:9000');
-}
+            // Design Request
+            var request = {
+                method: "GET",
+                url: "http://127.0.0.1:5000/get_image/" + count.toString()
+            }
 
-
-
-// Received new stream from server!
-client.on('stream', function(stream, meta){
-  // Buffer for parts
-  var parts = [];
-  // Got new data
-  stream.on('data', function(data){
-    parts.push(data);
-  });
-  stream.on('end', function(){
-    // Display new data in browser!
-    var img = document.createElement("img");
-    img.src = (window.URL || window.webkitURL).createObjectURL(new Blob(parts));
-    document.body.appendChild(img);
-  });
-});
+            // Send Request
+            $http(request).then(
+                // Success
+                function(response) {
+                    $scope.imgUrl = "http://127.0.0.1:5000/get_image/" + count.toString();
+                },
+                // Failure
+                function(response) {
+                    $scope.imgUrl = "http://127.0.0.1:5000/get_image/" + count.toString();
+                }
+            );
+            count = count + 1;
+        }, 10);
+    }
+);
---
+++ server.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask import send_file
+from flask import make_response
+
+app = Flask(__name__)
+
+#Alternating between 2 Images as a proof of concept
+@app.route("/get_image/<imgId>")
+def get_image(imgId):
+    global count
+    imgId = count
+    count += 1
+    if(count % 2 == 0):
+        response = make_response(send_file("A.gif", mimetype='image/gif'))
+        response.headers["imgId"] = imgId
+        return response
+    else:
+        response = make_response(send_file("B.gif", mimetype='image/gif'))
+        response.headers["imgId"] = imgId
+        return response
+
+if __name__ == "__main__":
+    global count
+    count = 0
+    app.run(debug=True)
